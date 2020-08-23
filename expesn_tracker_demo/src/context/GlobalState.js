b27838acfcdd6af8e451ff8b90059b34d755cce8
Ducmad <hieukio1159@gmail.com>
2020-08-23 07:53:13 +0700

    add format money func

--- expesn_tracker_demo/src/context/GlobalState.js
+++ expesn_tracker_demo/src/context/GlobalState.js
@@ -44,10 +44,25 @@ export const GlobalProvider=({children})=>{
             type:'CLEAR_TRANS'
         })
     }
-    
-    const format=(n)=> {
-        return n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-    }
+    const nFormatter=(num, digits=2)=> {
+        var si = [
+          { value: 1, symbol: "" },
+          { value: 1E3, symbol: "k" },
+          { value: 1E6, symbol: "M" },
+          { value: 1E9, symbol: "G" },
+          { value: 1E12, symbol: "T" },
+          { value: 1E15, symbol: "P" },
+          { value: 1E18, symbol: "E" }
+        ];
+        var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+        var i;
+        for (i = si.length - 1; i > 0; i--) {
+          if (num >= si[i].value) {
+            break;
+          }
+        }
+        return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+      }
     return (
         <GlobalContext.Provider value={{
             transactions: state.transactions,
@@ -55,7 +70,7 @@ export const GlobalProvider=({children})=>{
             addTransaction,
             clearTransaction,
             getTransaction,
-            format
+            format: nFormatter
         }}>
             {children}
         </GlobalContext.Provider>
